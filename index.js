a7ec677baf9b3b1169d9d05e1da1a092d17a19f4
Karl <92632850+Karlnjoku@users.noreply.github.com>
2022-02-26 16:27:14 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+myVar="pizza";
+
+switch(myVar){
+    case"A":
+        console.log("You done amazing");
+        break;
+    
+        case"B":
+            console.log("You done great");
+            break;
+
+            case"C":
+                console.log("You done Okay");
+                break;
+
+                case"D":
+                    console.log("You done bad");
+                    break;
+default:{
+    console.log(myVar,"is not a letter");
+}
+}
